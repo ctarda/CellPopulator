8e2ef272b2ceb09d3e1a1408d138396ff4ef5f47
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2019-06-16 08:54:49 +0800

    Extract some repetition

--- CellPopulator/ViewController.swift
+++ CellPopulator/ViewController.swift
@@ -46,22 +46,23 @@ private extension ViewController {
     private func prepareDataSource() {
         let data = DataModel().data()
         let dataManager = SectionedDataManager(data: data)
-        let coconutDataSource = DecorableDataSource(dataManager: dataManager,
-                                                    cellType: CoconutTableViewCell.self,
-                                                    populator: CoconutPopulator.self)
 
-        let developerDataSource = DecorableDataSource(dataManager: dataManager,
-                                                      cellType: DeveloperTableViewCell.self,
-                                                      populator: DeveloperPopulator.self,
-                                                      decorating: coconutDataSource)
+        let coconuts = buildDataSource(dataManager: dataManager, cellType: CoconutTableViewCell.self, populator: CoconutPopulator.self)
+        let developers = buildDataSource(dataManager: dataManager, cellType: DeveloperTableViewCell.self, populator: DeveloperPopulator.self, decorating: coconuts)
+        let cars = buildDataSource(dataManager: dataManager, cellType: CarTableViewCell.self, populator: CarPopulator.self, decorating: developers)
 
-        let carDataSource = DecorableDataSource(dataManager: dataManager,
-                                                cellType: CarTableViewCell.self,
-                                                populator: CarPopulator.self,
-                                                decorating: developerDataSource)
-
-        dataSource = carDataSource
+        dataSource = cars
 
         table.dataSource = dataSource
+    }
+
+    private func buildDataSource<Cell, Populator: CellPopulator>(dataManager: DataManager,
+                                                                                   cellType: Cell.Type,
+                                                                                   populator: Populator.Type,
+                                                                                   decorating: UITableViewDataSource? = nil) -> UITableViewDataSource where Populator.CellType == Cell {
+        return DecorableDataSource(dataManager: dataManager,
+                            cellType: cellType,
+                            populator: populator,
+                            decorating: decorating)
     }
 }
